7614f21d338d9881b58e3884b8fabe41b9fbe4cd
Hananiel Sarella <hananiel@gmail.com>
2019-05-09 17:41:01 -0400

    refactor

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -1,8 +1,8 @@
 ﻿import React, { Component } from 'react'; 
 import { InputSelector } from './InputSelector';
 import { RightInputSelector } from './RightInputSelector';
+import { AutoSuggestSelector } from './AutoSuggestSelector';
 //import { Suggestions } from './Suggestions';
-import { Typeahead } from 'react-bootstrap-typeahead'; 
 import './Home.css'
 
 // const hidden = {
@@ -18,38 +18,15 @@ export class Home extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            project: 'gradle-project',
-            dependencies: ['Hystrix', 'Actuator', 'MySql', 'Serilog'],
             showMore: false,
-            selected_deps: []
         }
-         this.handleSelection = this.handleSelection.bind(this);
          this.toggleMore = this.toggleMore.bind(this);
     }
-    
-    handleSelection(e) {
-        console.log("selection " + e);
-        //this.state.selected_deps.push(e);
-        this.setState(prevState => ({ selected_deps: [...prevState.selected_deps, e ]}))
-        this._typeAhead._instance.clear();
-
-        console.log('typeahead' , this._typeAhead);
-
-    }
+   
     toggleMore(e){
         this.setState(prevState => ({showMore: !prevState.showMore}))
     }
-    _renderMenuItemChildren = (option, props, index) => {
-        console.log(props)
-        return [
-           <div className="title">
-                {option}
-            </div>,
-            <div className="desc">
-                    {'This is the description for ' + option}
-              </div>
-        ];
-    }
+   
 
 
   render () {
@@ -91,53 +68,8 @@ export class Home extends Component {
                                                                             </div>
                                                                         </div>
                                                                     </div>
-                                                                    <div className="line">
-                                                                        <div className="left">
-                                                                            <div className="dependencies-label">Dependencies <a href={hrefLink} id="see-all">See all</a></div>
-                                                                        </div>
-                                                                        <div className="right">
-                                                                            <div className="colset">
-                                                                                <div className="col">Search dependencies to add
-                                <div className="control">
-                                    <Typeahead
-                                        id="typeahead"
-                                        className={'control-text'}
-                                        labelKey="name"
-                                        multiple={true}
-                                        options={this.state.dependencies}
-                                        renderMenuItemChildren={this._renderMenuItemChildren}
-                                        placeholder="Hystrix, Actuator ..."
-                                        onChange={this.handleSelection}
-                                        ref={(ref) => this._typeAhead = ref}
-                                    />
-</div>
-                                                                                        <div className="no-result" id="noresult-to-add"><em>No result.</em></div>
-                                <div className="list" id="list-to-add">
-                                  
-                                </div>
-                            </div>
-                          
-                                                                                    <div className="col" id="col-dep">
-                                                                                        <strong>Selected dependencies</strong>
-                                <div className="list light" id="list-added">
-                                    {
-                                        this.state.selected_deps.map((item) => {
-                                            return <div className="item" >
-                                                <div className="title">
-                                                    {item}
-                                                </div>
-                                                <div className="description">
-                                                    Some description about {item}
-                                                </div>
-                                            </div>
-                                        })
-
-                                    }
-                                </div>
-                                                                                    </div>
-                                                                                </div>
-                                                                            </div>
-                                                                        </div>
+                                   <AutoSuggestSelector />                           
+                                                                   
                                                                         <div className="line row-action">
                                                                             <div className="left">
                                                                                 <div className="footer">© 2013-2019 Pivotal Software<br/>start.steeltoe.io is powered by<br/>  
