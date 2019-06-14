143af85ca1d987ff779134a265eb491d0925ffde
hananiel <hananiel@gmail.com>
2019-06-13 20:04:29 -0400

    More UI changes; add list view

--- src/ClientApp/src/components/DependenciesSelector.js
+++ src/ClientApp/src/components/DependenciesSelector.js
@@ -4,15 +4,19 @@ import './DependenciesSelector.css'
 import { DependencyViewSelector } from './DependencyViewSelector';
 
 export class DependenciesSelector extends Component {
+    #MAX_ITEMS = 5;
     constructor(props) {
+        
         super(props);
         this.handleSelection = this.handleSelection.bind(this);
         this.handleViewChange = this.handleViewChange.bind(this);
+        this.toggleHover = this.toggleHover.bind(this);
         console.log('in constructor AutoSuggest');
         this.state = {
             dependencies:[],
             selected_deps: [],
-            activeView: 'quicksearch'
+            activeView: 'quicksearch',
+            hover: [false, false, false, false, false]
         }
         
     }
@@ -32,36 +36,71 @@ export class DependenciesSelector extends Component {
         this.setState({ activeView: e })
     }   
 
-    handleSelection(e) {
-        var currentSelection = e[0];
-        var selected_deps = [...this.state.selected_deps, currentSelection];
+    handleSelection(currentSelection) {
+        console.log(currentSelection)
+        var nextDeps =  this.state.dependencies.map(x => {
+            if (x.name == currentSelection.name) {
+                x.selected = currentSelection.selected != true;
+            }
+            return x;
+        });
         this.setState(prevState => ({
-            selected_deps: selected_deps,
-            dependencies: [...prevState.dependencies.filter(d => !selected_deps.some(sd => sd.name == d.name))]
+            dependencies: nextDeps//[...prevState.dependencies.filter(d => !selected_deps.some(sd => sd.name == d.name))]
 
         }))
-        this._typeAhead._instance.clear();
+        if (this._typeAhead) {
+            this._typeAhead._instance.clear();
+        }
 
-        //console.log('child state', selected_deps, this.state.selected_deps)
     }
-    removeDependency(dependency) {
-        var selected_deps = [...this.state.selected_deps.filter(d => d.name != dependency.name)]
-        this.setState(prevState => ({
-            selected_deps: selected_deps,
-            dependencies: [...prevState.dependencies, dependency].sort((a, b) => a.name > b.name)
-        }))
+    toggleHover(index) {
+    var newState = new Array(this.#MAX_ITEMS);
+        newState[index] = true;
+        this.setState({ hover: newState });
+        console.log(this.state.hover, index, this.state.hover[index]);
     }
+
     _renderMenuItemChildren = (option, props, index) => {
-         return [
-            <div className="title">
-                {option.name}
-            </div>,
-            <div key={index} className="desc">
-                    {option.description}
-              </div>
-        ];
+        const MAX_ITEMS = 5;
+        if (index < MAX_ITEMS) {
+            if (option.description.startsWith("Too many results found")) {
+                return [
+                    <div className="dependencies-list">
+                        <a className="dependency-item dependency-item-notice" href="/">
+                            <span className="description" key={index} className="desc">
+                                {option.description}
+                            </span>
+                        </a>
+                    </div>
+                ];
+            }
+            else {
+                return [
+                    <div className="dependencies-list">
+                        <a className={'dependency-item dependency-item-gray ' + (this.state.hover[index]? 'selected': '')} onMouseEnter={() => this.toggleHover(index)}  href="/">
+                            <strong className="title">
+                                {option.name}
+                            </strong>
+                            <br />
+                            <span className="description" key={index} className="desc">
+                                {option.description}
+                            </span>
+                            <span class="icon"><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="plus" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="icon-plus"><path fill="currentColor" d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"></path></svg></span>
+                        </a>
+                    </div>
+                ];
+            }
+        }
     }
-
+    getAvailableDeps(deps) {
+        
+        var available = deps.filter(d => !d.selected);
+        if (available.length > 4) {
+            available = available.slice(0, 4);
+            available.push({name: '', description: 'Too many results found. Start typing to refine your search.' })
+        }
+        return available;
+    }
 
     render() {
         let hrefLink = '#';
@@ -77,29 +116,35 @@ export class DependenciesSelector extends Component {
                             <div className="col">Search dependencies to add
                             <div className="control">
                                     <Typeahead
-                                        id="typeahead"
-                                        className={'control-text'}
-                                        labelKey="name"
-                                        multiple={true}
-                                        options={this.state.dependencies}
-                                        renderMenuItemChildren={this._renderMenuItemChildren}
-                                        placeholder="Hystrix, Actuator ..."
-                                        onChange={this.handleSelection}
-                                        ref={(ref) => this._typeAhead = ref} />
+                                    id="typeahead"
+                                    className={'control-text'}
+                                    labelKey="name"
+                                    maxHeight={'500px'}
+                                    multiple={true}
+                                    options={this.getAvailableDeps(this.state.dependencies)}
+                                    renderMenuItemChildren={this._renderMenuItemChildren}
+                                    placeholder="Circuit Breaker, Actuator ..."
+                                    onChange={(e) => { this.handleSelection(e[0]) }}
+                                    ref={(ref) => this._typeAhead = ref}
+                                />
                                 </div>
                             </div>
                             <div className="col" id="col-dep" >
-                                <strong>Selected dependencies</strong>
-                                <div className="selectedDeps light" id="list-added">
-                                    <input name="dependencies" type="hidden" defaultValue={this.state.selected_deps.map(d => d.name)} />
+                               Selected dependencies
+                            <div className="dependencies-list dependencies-list-checked" id="list-added">
+                                <input name="dependencies" type="hidden" defaultValue={this.state.dependencies.filter(d => d.selected === true).map(d => d.name)} />
 
-                                    {
-                                        this.state.selected_deps.map((item) => {
-                                            return <div className="item" >
-                                                <div className="title"> {item.name} </div>
-                                                <div className="description"> {item.description} </div>
-                                                <a class="btn-ico" onClick={() => { this.removeDependency(item) }}><i class="fas fa-times"></i></a>
-                                            </div>
+                                {
+                                    this.state.dependencies.filter(d => d.selected === true).map((item) => {
+                                        return <a className="dependency-item checked" onClick={() => { this.handleSelection(item) }}>
+                                                <div>
+                                                    <strong> {item.name} </strong>
+                                                    <br/>
+                                                <span className="description"> {item.description} </span>
+                                                    
+                                                <span class="icon"><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="times" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512" class="icon-times"><path fill="currentColor" d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"></path></svg><svg aria-hidden="true" focusable="false" data-icon="check" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="icon-check"><path fill="currentColor" d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"></path></svg></span>
+                                                </div>
+                                                </a>
                                         })
 
                                     }
@@ -113,7 +158,7 @@ export class DependenciesSelector extends Component {
                                 <div className="group-items">
                                 {
                                     this.state.dependencies.map((option, index) => {
-                                        return <a className="">
+                                        return <a className={option.selected == true ? 'checked' : ''} onClick={() => { this.handleSelection(option) }} >
                                             <div>
                                                 <strong>{option.name}</strong><br />
                                                 <span>{option.description}</span>
