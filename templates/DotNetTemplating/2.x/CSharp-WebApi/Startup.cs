9d47bcf00d8e0f5187143780d3d50e050682202f
Hananiel Sarella <hsarella@pivotal.io>
2019-11-15 11:45:54 +0530

    Dev (#56)
    * Improve integration tests; fix combinations in templates
    
    * fix pipeline
    
    * Issue with integration tests on build server
    
    * Add 2.4.0-rc1
    
    * Add IntegrationTests back; fix default
    
    * add 2.4.0

--- templates/DotNetTemplating/2.x/CSharp-WebApi/Startup.cs
+++ templates/DotNetTemplating/2.x/CSharp-WebApi/Startup.cs
@@ -25,7 +25,7 @@ using Microsoft.Extensions.Options;
 #if (Actuators || CloudFoundry)
 using Steeltoe.Management.CloudFoundry;
 using Steeltoe.Management.Endpoint;
-#if (SteeltoeVersion == "2.2.0" || SteeltoeVersion == "2.3.0" || SteeltoeVersion == "2.4.0-rc1")
+#if (SteeltoeVersion == "2.2.0" || SteeltoeVersion == "2.3.0" || SteeltoeVersion == "2.4.0")
 using Steeltoe.Management.Hypermedia;
 #endif
 #endif
@@ -86,7 +86,7 @@ namespace Company.WebApplication1
 #if (MySql)
             services.AddMySqlConnection(Configuration);
 #endif
-#if (SteeltoeVersion == "2.2.0" || SteeltoeVersion == "2.3.0" || SteeltoeVersion == "2.4.0-rc1")
+#if (SteeltoeVersion == "2.2.0" || SteeltoeVersion == "2.3.0" || SteeltoeVersion == "2.4.0")
 #if (Actuators && CloudFoundry)
 	        services.AddCloudFoundryActuators(Configuration, MediaTypeVersion.V2, ActuatorContext.ActuatorAndCloudFoundry);
 #elif (Actuators)
@@ -160,7 +160,7 @@ namespace Company.WebApplication1
 #if (OrganizationalAuth || IndividualAuth)
             app.UseAuthentication();
 #endif
-#if (SteeltoeVersion == "2.2.0" || SteeltoeVersion == "2.3.0" || SteeltoeVersion == "2.4.0-rc1")
+#if (SteeltoeVersion == "2.2.0" || SteeltoeVersion == "2.3.0" || SteeltoeVersion == "2.4.0")
 #if (Actuators && CloudFoundry)
             app.UseCloudFoundryActuators(MediaTypeVersion.V2, ActuatorContext.ActuatorAndCloudFoundry);
 #elif (Actuators)
