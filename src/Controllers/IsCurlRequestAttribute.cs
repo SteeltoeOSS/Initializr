9da0e6206f812274d7d47ba38bca6bbe5179b5dd
Hananiel Sarella <hsarella@pivotal.io>
2019-12-11 18:12:27 -0500

    Fix Validation (#59)
    * Fix validation

---
+++ src/Controllers/IsCurlRequestAttribute.cs
@@ -0,0 +1,33 @@
+﻿// Copyright 2017 the original author or authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.ActionConstraints;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Steeltoe.Initializr.Controllers
+{
+    public class IsCurlRequestAttribute : ActionMethodSelectorAttribute
+    {
+        public override bool IsValidForRequest(RouteContext routeContext, ActionDescriptor action)
+        {
+            var headers = routeContext.HttpContext.Request.Headers;
+            return headers["User-Agent"].Any(h => h.ToLower().Contains("curl"));
+        }
+    }
+}
