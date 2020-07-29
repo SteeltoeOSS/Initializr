5cd7ddfd5bbcca98191df341d4a5cb50510b458e
Chris Cheetham <chris@cheetham.com>
2020-07-29 09:56:39 -0400

    Update copyright
    Also, remove "powered by"

--- src/Steeltoe.Initializr.WebApp/ClientApp/src/components/BottomLinks.js
+++ src/Steeltoe.Initializr.WebApp/ClientApp/src/components/BottomLinks.js
@@ -6,8 +6,7 @@ export class BottomLinks extends Component {
     return (
 
     <div className="left">
-        <div className="footer">© 2013-2020 Pivotal Software<br/>start.steeltoe.io is powered by<br/>
-            <a href="https://run.pivotal.io/" tabIndex="999">Pivotal Web Services</a><br/>v 0.6.2</div>
+        <div className="footer">© 2019-2020 VMware, Inc. All Rights Reserved<br/>v 0.6.2</div>
     </div>
     );
   }
