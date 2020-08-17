1a90e0be859e04818e153ac4b3998eac978750db
Chris Cheetham <chris@cheetham.com>
2020-08-17 10:28:09 -0400

    Restore netcoreapp3.1 templates for Steeltoe 2.4

--- src/templates/Mustache/2.4/netcoreapp3.1/Steeltoe-WebApi/Startup.cs
+++ src/templates/Mustache/2.4/netcoreapp3.1/Steeltoe-WebApi/Startup.cs
@@ -1,44 +1,13 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-{{#MongoDB}}
-using Steeltoe.Connector.MongoDb;
-{{/MongoDB}}
-{{#MySqlOrMySqlEFCore}}
-using Steeltoe.Connector.MySql;
-{{/MySqlOrMySqlEFCore}}
-{{#MySqlEFCore}}
-using Steeltoe.Connector.MySql.EFCore;
-{{/MySqlEFCore}}
-{{#OAuthConnector}}
-using Steeltoe.Connector.OAuth;
-{{/OAuthConnector}}
-{{#Postgres}}
-using Steeltoe.Connector.PostgreSql;
-{{/Postgres}}
-{{#PostgresEFCore}}
-using Steeltoe.Connector.PostgreSql.EFCore;
-{{/PostgresEFCore}}
-{{#RabbitMQ}}
-using Steeltoe.Connector.RabbitMQ;
-{{/RabbitMQ}}
-{{#Redis}}
-using Steeltoe.Connector.Redis;
-{{/Redis}}
-{{#SQLServer}}
-using Steeltoe.Connector.SqlServer.EFCore;
-{{/SQLServer}}
-{{#Discovery}}
-using Steeltoe.Discovery.Client;
-{{/Discovery}}
-{{#Actuators}}
-using Steeltoe.Management.CloudFoundry;
-{{/Actuators}}
 {{#RequiresHttps}}
 using Microsoft.AspNetCore.HttpsPolicy;
 {{/RequiresHttps}}
+using Microsoft.AspNetCore.Mvc;
 {{#Auth}}
 using Microsoft.AspNetCore.Authentication;
 {{/Auth}}
@@ -48,10 +17,51 @@ using Microsoft.AspNetCore.Authentication.AzureAD.UI;
 {{#IndividualB2CAuth}}
 using Microsoft.AspNetCore.Authentication.AzureADB2C.UI;
 {{/IndividualB2CAuth}}
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Hosting;
+
+{{#ActuatorsOrCloudFoundry}}
+using Steeltoe.Management.CloudFoundry;
+using Steeltoe.Management.Endpoint;
+using Steeltoe.Management.Hypermedia;
+{{/ActuatorsOrCloudFoundry}}
+
 {{#CircuitBreaker}}
 using Steeltoe.CircuitBreaker.Hystrix;
 {{/CircuitBreaker}}
-
+{{#MySqlOrMySqlEFCore}}
+using Steeltoe.CloudFoundry.Connector.MySql;
+{{/MySqlOrMySqlEFCore}}
+{{#MySqlEFCore}}
+using Steeltoe.CloudFoundry.Connector.MySql.EFCore;
+{{/MySqlEFCore}}
+{{#SQLServer}}
+using Steeltoe.CloudFoundry.Connector.SqlServer;
+{{/SQLServer}}
+{{#Discovery}}
+using Steeltoe.Discovery.Client;
+{{/Discovery}}
+{{#Postgres}}
+using Steeltoe.CloudFoundry.Connector.PostgreSql;
+{{/Postgres}}
+{{#RabbitMQ}}
+using Steeltoe.CloudFoundry.Connector.RabbitMQ;
+{{/RabbitMQ}}
+{{#Redis}}
+using Steeltoe.CloudFoundry.Connector.Redis;
+{{/Redis}}
+{{#MongoDB}}
+using Steeltoe.CloudFoundry.Connector.MongoDb;
+{{/MongoDB}}
+{{#OAuthConnector}}
+using Steeltoe.CloudFoundry.Connector.OAuth;
+{{/OAuthConnector}}
+{{#PostgresEFCore}}
+using Steeltoe.CloudFoundry.Connector.PostgreSql.EFCore;
+{{/PostgresEFCore}}
 namespace {{ProjectNameSpace}}
 {
     public class Startup
@@ -79,10 +89,10 @@ namespace {{ProjectNameSpace}}
 {{/MySql}}
 {{#Actuators}}
 {{#CloudFoundry}}
-	        services.ConfigureCloudFoundryOptions(Configuration, MediaTypeVersion.V2, ActuatorContext.ActuatorAndCloudFoundry);
+	        services.AddCloudFoundryActuators(Configuration, MediaTypeVersion.V2, ActuatorContext.ActuatorAndCloudFoundry);
 {{/CloudFoundry}}
 {{^CloudFoundry}}
-            services.AddCloudFoundryActuators(Configuration);
+	        services.AddCloudFoundryActuators(Configuration);
 {{/CloudFoundry}}
 {{/Actuators}}
 {{#Discovery}}
@@ -96,16 +106,19 @@ namespace {{ProjectNameSpace}}
 {{/RabbitMQ}}
 {{#Redis}}
             // Add the Redis distributed cache.
+
             // We are using the Steeltoe Redis Connector to pickup the CloudFoundry
             // Redis Service binding and use it to configure the underlying RedisCache
             // This adds a IDistributedCache to the container
             services.AddDistributedRedisCache(Configuration);
+
             // This works like the above, but adds a IConnectionMultiplexer to the container
             // services.AddRedisConnectionMultiplexer(Configuration);
 {{/Redis}}
 {{#MongoDB}}
             services.AddMongoClient(Configuration);
 {{/MongoDB}}
+
 {{#OAuthConnector}}
             services.AddOAuthServiceOptions(Configuration);
 {{/OAuthConnector}}
@@ -113,9 +126,12 @@ namespace {{ProjectNameSpace}}
             // Add Context and use Postgres as provider ... provider will be configured from VCAP_ info
             // services.AddDbContext<MyDbContext>(options => options.UseNpgsql(Configuration));
 {{/PostgresEFCore}}
+{{#SQLServer}}
+            services.AddSqlServerConnection(Configuration);
+{{/SQLServer}}
             services.AddControllers();
         }
-
+ 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
@@ -123,7 +139,7 @@ namespace {{ProjectNameSpace}}
             {
                 app.UseDeveloperExceptionPage();
             }
-{{#RequiresHttps}}
+            {{#RequiresHttps}}
             else
             {
             // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
@@ -131,14 +147,25 @@ namespace {{ProjectNameSpace}}
             }
 
             app.UseHttpsRedirection();
-{{/RequiresHttps}}
-{{#Auth}}
-            app.UseAuthentication();
-{{/Auth}}
+            {{/RequiresHttps}}
 
-{{#Discovery}}
+
+            {{#Auth}}
+            app.UseAuthentication();
+            {{/Auth}}
+
+            {{#Actuators}}
+            {{#CloudFoundry}}
+            app.UseCloudFoundryActuators(MediaTypeVersion.V2, ActuatorContext.ActuatorAndCloudFoundry);
+            {{/CloudFoundry}}
+            {{^CloudFoundry}}
+            app.UseCloudFoundryActuators();
+            {{/CloudFoundry}}
+            {{/Actuators}}
+       
+            {{#Discovery}}
             app.UseDiscoveryClient();
-{{/Discovery}}
+            {{/Discovery}}
             app.UseRouting();
             app.UseEndpoints(endpoints =>
             {
