0bc43db34dc699ef04e56437d9aa1c4627516d41
Chris Cheetham <chris@cheetham.com>
2020-08-19 14:00:44 -0400

    Bump Steeltoe 3.0.0-rc1 to 3.0.0 [#123]

--- src/Steeltoe.Initializr.TemplateEngine/Constants.cs
+++ src/Steeltoe.Initializr.TemplateEngine/Constants.cs
@@ -3,7 +3,7 @@ namespace Steeltoe.Initializr.TemplateEngine
     public static class Constants
     {
         public const string Steeltoe24 = "2.4.4";
-        public const string Steeltoe30 = "3.0.0-rc1";
+        public const string Steeltoe30 = "3.0.0";
         public const string NetCoreApp21 = "netcoreapp2.1";
         public const string NetCoreApp31 = "netcoreapp3.1";
         public const string WebApi = "webapi";
